e0d9cfe533dce52987fa314bf0db7e4ada155039
PERFEST <perfect2111@gmail.com>
2020-09-21 16:23:11 +0300

    this will start travis

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -3,7 +3,8 @@
 
 
 int main()
-{	
+{
+    // comment for travis
 	std::cout << "2 + 6 = " << sum(2, 6) << std::endl;
 	std::cout << "4 - 2 = " << dif(4, 2) << std::endl;
 	std::cout << "3 * 6 = " << mul(3, 6) << std::endl; 
